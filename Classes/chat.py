a963b4c632a33a9862789921c2313673deb7528c
yeeyangtee <yeeyang95@gmail.com>
2022-12-05 18:23:43 -0600

    test

--- Classes/chat.py
+++ Classes/chat.py
@@ -8,7 +8,7 @@ from typing import Tuple
 import requests
 
 
-def ask(auth_token: str, prompt, previous_convo_id: str or None) -> Tuple[str, str or None]:
+def ask(auth_token: str, prompt, conversation_id:str or None, previous_convo_id: str or None) -> Tuple[str, str or None]:
     headers = {
         "Accept": "application/json",
         "Authorization": f"Bearer {auth_token}",
@@ -26,7 +26,7 @@ def ask(auth_token: str, prompt, previous_convo_id: str or None) -> Tuple[str, s
                 "content": {"content_type": "text", "parts": [prompt]}
             }
         ],
-        "conversation_id": None,
+        "conversation_id": conversation_id,
         "parent_message_id": previous_convo_id,
         "model": "text-davinci-002-render"
     }
@@ -40,15 +40,15 @@ def ask(auth_token: str, prompt, previous_convo_id: str or None) -> Tuple[str, s
             response_text = response.text.replace("data: [DONE]", "")
             data = re.findall(r'data: (.*)', response_text)[-1]
             as_json = json.loads(data)
-            return as_json["message"]["content"]["parts"][0], as_json["conversation_id"]
+            return as_json["message"]["content"]["parts"][0], as_json["message"]["id"], as_json["conversation_id"]
         elif response.status_code == 401:
             print("Error: " + response.text)
-            return "401", None
+            return "401", None, None
         else:
             print("Error: " + response.text)
-            return "Error", None
+            return "Error", None, None
     except Exception as e:
         print(">> Error when calling OpenAI API: " + str(e))
-        return "400", None
+        return "400", None, None
 
 
--- main.py
+++ main.py
@@ -53,6 +53,7 @@ if __name__ == "__main__":
 
     print(f"{Fore.GREEN}>> Starting chat..." + Fore.RESET)
     previous_convo_id = None
+    conversation_id = None
     access_token = Auth.get_access_token()
     while True:
         try:
@@ -61,8 +62,9 @@ if __name__ == "__main__":
                 exit(1)
 
             user_input = input("You: ")
-            answer, previous_convo = Chat.ask(auth_token=access_token,
+            answer, previous_convo, conversation = Chat.ask(auth_token=access_token,
                                               prompt=user_input,
+                                              conversation_id=conversation_id,
                                               previous_convo_id=previous_convo_id)
             if answer == "400" or answer == "401":
                 print(f"{Fore.RED}>> Your token is invalid. Attempting to refresh..")
@@ -73,6 +75,8 @@ if __name__ == "__main__":
             else:
                 if previous_convo is not None:
                     previous_convo_id = previous_convo
+                if conversation is not None:
+                    conversation_id = conversation
 
                 print(f"Chat GPT: {answer}")
         except KeyboardInterrupt:
